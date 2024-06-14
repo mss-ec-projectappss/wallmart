b2b34a4108518f6277cd0234edc69dd91a8a24f2
krish <bkrishna0055@gmail.com>
2024-06-14 09:46:05 +0530

    new-commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+
+echo "Welcoem to devops"
+echo "Display the today date"
+date
---
+++ users.sh
@@ -0,0 +1,2 @@
+Hello team,
+this is inform you that all of you should be there in the seassion and make it as completed.
