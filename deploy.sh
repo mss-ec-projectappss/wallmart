f406d065e19ef27b882940b6b6f18d3cdc6460c4
krish <bkrishna0055@gmail.com>
2024-06-14 09:59:02 +0530

    last-commit

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,13 @@ echo "display the calender"
 cal
 echo "added new code"
 <<<<<<< HEAD
+<<<<<<< HEAD
 echo "idisplay	:wq"
 echo "Hi I'm from dev b"
+=======
+display	:wq
+
+>>>>>>> 34eb05d (added)
 =======
 display	:wq
 
