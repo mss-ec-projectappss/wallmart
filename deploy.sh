2c2b590126d56fc80cc5689a0d34a67168a92d0a
krish <bkrishna0055@gmail.com>
2024-06-14 09:59:02 +0530

    last-modified

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,10 @@ echo "display the content"
 echo "display the calender"
 cal
 echo "added new code"
+<<<<<<< HEAD
 echo "idisplay	:wq"
 echo "Hi I'm from dev b"
+=======
+display	:wq
+
+>>>>>>> 34eb05d (added)
