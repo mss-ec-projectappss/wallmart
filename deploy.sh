71d785092422e989c3c054413ebfbd3d78bf0f53
krish <bkrishna0055@gmail.com>
2024-06-15 11:24:04 +0530

    nasdf

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,11 @@ echo "display the content"
 echo "display the calender"
 cal
 echo "added new code"
+<<<<<<< HEAD
 echo "idisplay	:wq"
 echo "Hi I'm from dev b"
+=======
+display	:wq
+Hi I'm from dev branch
+>>>>>>> 5198f36 (addedcode)
 echo "hello.sh file is required"
